24b9e18289b56cc7999ee116d31059b78373ce78
Vekisa <vmosorinski@gmail.com>
2019-02-02 20:04:07 +0100

    front i neke stvari na backu

--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -1,12 +1,56 @@
+function load(){
+	$.ajax({
+		type: "GET",
+		url: "users/registered/currentUser",
+		beforeSend: function(xhr) {
+	          if (localStorage.token) {
+	            xhr.setRequestHeader('X-Auth-Token', localStorage.token);
+	            
+	          }
+	    },
+		success: function(data){
+			if(data!=null){
+				$('#user').val(data.username);
+			}
+		}
+	});
+	
+	$('#profile').hide();
+}
+
+$(document).on('click','#user',function(e){
+	e.preventDefault();
+	$("#").hide();
+	$("#profile").show();
+	$("#tablediv").hide();
+	
+});
+
+$(document).ajaxError(function( event, jqxhr, settings, thrownError ) {
+	  
+    switch(jqxhr.status) {
+	    case 404:
+	    	 $("#error").html("Not found");
+	      break;
+	    case 204:
+	    	$("#error").html("Not found");
+	      break;
+	    case 400:
+	    	$("#error").html("Bad request");
+	      break;
+	    default:
+	    	$("#error").html("Something went wrong");
+    }
+    $('#myModal').modal("show");
+});
+
 $(document).on('click','#logout',function(e){
 	e.preventDefault();
 	$.ajax({
 		url: "/users/registered/logout",
 		type:"POST",
 		async: false,
-		contentType:"application/json",
-		dataType:"json",
-		complete: function(data){
+		success: function(data){
 			localStorage.token = null;
 			window.location.href = 'index.html';		
 		},
