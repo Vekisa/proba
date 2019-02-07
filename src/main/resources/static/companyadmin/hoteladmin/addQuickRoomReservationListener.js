6594fcc2212ad1a43cadfe5cf0ccdf0b5f29683b
djuka996 <aleksamdjurjulov@gmail.com>
2019-02-07 02:31:21 +0100

    Ispravka brze rezervacije

--- src/main/resources/static/companyadmin/hoteladmin/addQuickRoomReservationListener.js
+++ src/main/resources/static/companyadmin/hoteladmin/addQuickRoomReservationListener.js
@@ -10,6 +10,7 @@ $(document).on('submit', '#quickRoomForm', function(e) {
 	let jsonBody = JSON.stringify(newRoomReservation);
 	let roomId = $("#quickRoomSelect option:selected").val();
 	let url = "/room_reservations/quicks?room=" + roomId;
+	let reservationId;
 	$.ajax({
 		type: "POST",
 		url: url,
@@ -20,10 +21,30 @@ $(document).on('submit', '#quickRoomForm', function(e) {
 		beforeSend: function(request) {
     		request.setRequestHeader("X-Auth-Token", localStorage.getItem("token"));
   		},
-  		success: function() {
-  			loadCollection();
+  		success: function(data) {
+  			reservationId = data.id;
   		}
 	});
+	var selects = document.getElementsByClassName("cbExtraOptions");
+    var extOpt = new Array();
+    $.each(selects, function(index, select){
+            if(select.checked == true)
+                extOpt.push(select.value);
+    });
+    $.ajax({
+    	type: "POST",
+    	url: "/room_reservations/" + reservationId + "/multiple_extra_options",
+		dataType: "json",
+		data: JSON.stringify(extOpt),
+		contentType: "application/json",
+		async: false,
+		beforeSend: function(request) {
+    		request.setRequestHeader("X-Auth-Token", localStorage.getItem("token"));
+  		},
+  		success: function(data) {
+  			loadCollection();
+  		}
+    });
 })
 
 $(document).on('click', '#addQuickRoom', function(e) {
@@ -52,10 +73,24 @@ function createQuickRoomForm() {
   		}
 	});
 	selectRoomHTML += "</select><br><br>";
+	let extraOptions = "<div aria-labelledby=\"dropdownMenuLink\" id=\"extraOptionsList\"></div><br>";
+	let selectExtraOptionHTML = "";
+    $.ajax({
+		url: "/hotels/" + companyId + "/extra-options",
+		type:"GET",
+        async: false,
+		success: function(data){
+            $.each(data,function(index,extraOption){
+              selectExtraOptionHTML += "<input type=\"checkbox\" class=\"cbExtraOptions\" name=\"" + index + "\" value=\"" + extraOption.id + "\"" + index + "\">" + extraOption.description + "  " + extraOption.pricePerDay +"&euro;" + "<br>"; 
+            });     
+		}
+	});
 	let submitHTML = "<input type=\"submit\" class=\"btn btn-info btn-block custombutton\" value=\"Add\">";
 	$("#quickRoomForm").append(beginDateHTML);
 	$("#quickRoomForm").append(endDateHTML);
 	$("#quickRoomForm").append(selectRoomHTML);
+	$("#quickRoomForm").append(extraOptions);
 	$("#quickRoomForm").append(submitHTML);	
-	$("collection").show();
+	$("#collection").show();
+	document.getElementById('extraOptionsList').innerHTML = selectExtraOptionHTML;
 }
